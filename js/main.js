25419b7330d31548daaca4776030bd0ed4ba0e3b
Dzmitry Datskevich <dda.enigmer@gmail.com>
2020-11-19 16:03:27 +0300

    fix: change code styles

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
-const panels = document.querySelectorAll('.panel');
+const panels = document.querySelectorAll(".panel");
 
 function toggleOpen() {
-  panels.forEach(panel => panel.classList.remove("open"));
+  panels.forEach((panel) => panel.classList.remove("open"));
   this.classList.add("open");
 }
 
+function toggleActive(e) {
+  if (e.propertyName.includes("flex")) {
+    this.classList.toggle("open-active");
+  }
+}
 
-    function toggleActive(e) {
-      if (e.propertyName.includes('flex')) {
-        this.classList.toggle('open-active');
-      }
-    }
-
-    panels.forEach(panel => panel.addEventListener('click', toggleOpen));
-    panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+panels.forEach((panel) => panel.addEventListener("click", toggleOpen));
+panels.forEach((panel) =>
+  panel.addEventListener("transitionend", toggleActive)
+);
